2dce5f15a32ad2a4c40d615d0c2bd0d34fc178b0
SofiKor <sofiakorelskaya@mail.ru>
2024-07-24 01:11:01 +0300

    Add files via upload

---
+++ lesson1.py
@@ -0,0 +1,8 @@
+#1st program
+print(9**0.5*5)
+#2st program
+print(9.99>9.98 and 1000!= 1000.1)
+#3rd program
+print(((1234//10)%100)+((5678//10)%100))
+#4th program
+print(bool(int(13.42-0.42))==(float(42.13//3.24) and (int(42.13-0.13))==(int(13.42//0.319))))
